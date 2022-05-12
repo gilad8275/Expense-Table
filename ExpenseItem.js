90cb42ae425b1e4c11ce13139e01b7834d72c9f9
gilad8275 <94356949+gilad8275@users.noreply.github.com>
2022-05-12 12:20:31 +0300

    Add files via upload
    The first structure for my app.

---
+++ ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  return (
+    <div className='expense-item'>
+      <div>Date</div>
+      <div className='expense-item__description'>
+        <h2>Description</h2>
+        <div className='expense-item__price'>Price</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
